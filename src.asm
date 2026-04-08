d0c8b1c666ed3fb27ada69cf1d57fd7266e6b3ba
ryo11aori-ship-it <ryo11aori@gmail.com>
2026-04-08 19:14:48 +0900

    src.asm を更新

--- src.asm
+++ src.asm
@@ -1,5 +1,20 @@
-# Inceptionut Bootstrap Assembly
-# A B C
-3 2 1
-# Infinite Loop
-0 0 0
+# --- Inceptionut Bootstrapper & Echo ---
+
+# Phase 1: Dimensional Trampoline (Ignition)
+# Dim 0を埋め尽くしてBig Bangを意図的に誘発する。
+# Dim 1拡張後、PC=[0,0]がこれを読むと
+# Subleq([9,1], [9,1], [2,1]) として解釈され、[2,1]へ安全にジャンプする。
+9 1 9 1 2 1 1 1 1 1
+
+# Phase 2: Dim 1 Echo Program
+# [0,1]と[1,1]はジャンプで飛び越えるためのパディング
+0 0
+
+# Addr [2,1]: IN(9,9) -> Temp(7,1). Jump to [5,1]
+9 9 7 1 5 1
+
+# Addr [5,1]: Temp(7,1) -> OUT(9,9). Jump to [8,1]
+7 1 9 9 8 1
+
+# Addr [8,1]: Halt (Infinite Loop in [8,1])
+8 1 8 1 8 1
