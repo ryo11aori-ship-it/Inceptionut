d2f5a77d3675db2862cf6d77cf373075e6f69dde
ryo11aori-ship-it <ryo11aori@gmail.com>
2026-04-08 19:35:56 +0900

    src.asm を更新

--- src.asm
+++ src.asm
@@ -1,20 +1,23 @@
-# --- Inceptionut Bootstrapper & Echo ---
+# --- Inceptionut Ultimate Echo (Double Fallthrough) ---
 
-# Phase 1: Trampoline (Ignition)
-9 1 9 1 2 1 1 1 1 1
+# [Bootloader Ignition]
+# Jumps safely to [0,1]
+9 1 9 1 0 1 1 1 1 1
 
-# Phase 2: Dim 1 Space
-# Padding: [0,1] and [1,1]
-0 0
+# [Block 1: IN] at [0,1]
+# Temp([8,8]) = Temp - IN([9,9]). Jumps to OUT([4,3])
+9 9 8 8 4 3 0 0 0 0
 
-# Inst 1 at [2,1]: Temp([8,1]) = Temp - IN([9,9]). Jump to [0,2]
-9 9 8 1 0 2
+# [Block 2: CLEAR] at [0,2]
+# Temp = Temp - Temp. Jumps to IN([0,1])
+8 8 8 8 0 1 0 0 0 0
 
-# Padding: [8,1] is Temp. [9,1] is unused.
-0 0
+# [Block 3: DUMMY & OUT] at [0,3]
+# Inst 1 at [4,3]: OUT. Falls through to [0,3].
+# Inst 2 at [0,3]: Dummy(1 - 0 > 0). Falls through to [6,3].
+# Inst 3 at [6,3]: Wrapped Jump. Jumps to [6,4]!
+6 4 5 4 8 8 9 9 9 8
 
-# Inst 2 at [0,2]: OUT([9,9]) = OUT - Temp([8,1]). Does not jump.
-8 1 9 9 6 2
-
-# Inst 3 at [6,2]: Clear Temp([8,1]) = Temp - Temp. Jump to [2,1]
-8 1 8 1 2 1
+# [Block 4: TRAMPOLINE] at [0,4]
+# Inst 1 at [6,4]: Jump to CLEAR([0,2])
+0 2 0 0 0 1 0 0 0 0
