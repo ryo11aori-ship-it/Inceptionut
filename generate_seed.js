7d1d2ebb6ca7d29f44a170533bfc3c1bc833b009
ryo11aori-ship-it <ryo11aori@gmail.com>
2026-04-09 23:04:47 +0900

    generate_seed.js を更新

--- generate_seed.js
+++ generate_seed.js
@@ -1,21 +1,9 @@
 // generate_seed.js
 const fs=require('fs');
-class Dim2Builder{
+class Dim0Builder{
 constructor(){
 this.universe=new Array(10).fill(null).map(()=>new Array(10).fill(null).map(()=>new Array(10).fill(0)));
 this.invisibleMap=["    ","   \t","  \t ","  \t\t"," \t  "," \t \t"," \t\t "," \t\t\t","\t   ","\t  \t"];
-}
-emit(z,y,ax,ay,az,bx,by,bz,cx,cy,cz,w9=0){
-this.universe[z][y][0]=ax;
-this.universe[z][y][1]=ay;
-this.universe[z][y][2]=az;
-this.universe[z][y][3]=bx;
-this.universe[z][y][4]=by;
-this.universe[z][y][5]=bz;
-this.universe[z][y][6]=cx;
-this.universe[z][y][7]=cy;
-this.universe[z][y][8]=cz;
-this.universe[z][y][9]=w9;
 }
 simulate(inputChars){
 console.error("[Simulator] Starting Internal Verification...");
@@ -24,29 +12,31 @@ let inputs=inputChars.split('').map(c=>c.charCodeAt(0));
 let inputPtr=0;
 let cycles=0;
 const advancePC=()=>{
-let val=this.universe[pc[2]][pc[1]][pc[0]];
+let val=this.universe[0][0][pc[0]];
 pc[0]++;if(pc[0]>9)pc[0]=0;
 return val;
 };
+const getTorus=(c)=>{
+let v=c%10;return v<0?v+10:v;
+};
 while(cycles<1000){
-let currentPC=[...pc];
-let ax=advancePC();let ay=advancePC();let az=advancePC();
-let bx=advancePC();let by=advancePC();let bz=advancePC();
-let cx=advancePC();let cy=advancePC();let cz=advancePC();
-let valA=(ax===9&&ay===9&&az===9)?(inputPtr<inputs.length?inputs[inputPtr++]:-1):this.universe[az][ay][ax];
-let valB=(bx===9&&by===9&&bz===9)?0:this.universe[bz][by][bx];
+let ax=advancePC();
+let bx=advancePC();
+let cx=advancePC();
+let valA=(ax===9)?(inputPtr<inputs.length?inputs[inputPtr++]:-1):this.universe[0][0][getTorus(ax)];
+let valB=(bx===9)?0:this.universe[0][0][getTorus(bx)];
 let res=valB-valA;
-if(bx===9&&by===9&&bz===9){
-console.error(`[Simulator] Output: ${res&255}`);
+if(bx===9){
+console.error(`[Simulator] Output: ${String.fromCharCode(res&255)}`);
 }else{
-this.universe[bz][by][bx]=res;
+this.universe[0][0][getTorus(bx)]=res;
 }
 if(res<=0){
-if(currentPC[0]===cx&&currentPC[1]===cy&&currentPC[2]===cz){
-console.error("[Simulator] SUCCESS: Expected Halt Reached.");
+if(cx<0){
+console.error("[Simulator] SUCCESS: Negative Address Halt.");
 return true;
 }
-pc=[cx,cy,cz];
+pc=[cx,0,0];
 }
 cycles++;
 }
@@ -66,10 +56,17 @@ console.error("[Simulator] Binary forged and verified.");
 }
 }
 function main(){
-let b=new Dim2Builder();
-b.emit(0,0,9,9,9,9,9,9,0,1,0,0);
-b.emit(0,1,0,0,0,0,0,0,0,0,0,0);
-b.emit(1,0,0,0,0,0,0,9,0,1,0,0);
+let b=new Dim0Builder();
+b.universe[0][0][0]=1;
+b.universe[0][0][1]=1;
+b.universe[0][0][2]=3;
+b.universe[0][0][3]=9;
+b.universe[0][0][4]=1;
+b.universe[0][0][5]=7;
+b.universe[0][0][6]=2;
+b.universe[0][0][7]=1;
+b.universe[0][0][8]=9;
+b.universe[0][0][9]=0;
 try{
 b.simulate("019");
 b.build();
