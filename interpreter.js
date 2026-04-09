05c9b3d4df647f259121db5b728b2ad486a0a9cb
ryo11aori-ship-it <ryo11aori@gmail.com>
2026-04-09 22:28:49 +0900

    interpreter.js を更新

--- interpreter.js
+++ interpreter.js
@@ -1,163 +1,51 @@
 const fs=require('fs');
-let maxDim=0;
-let universe=[0,0,0,0,0,0,0,0,0,0];
-function checkSat(arr,dim){
-if(dim===0){
-for(let i=0;i<10;i++)if(arr[i]===0)return false;
-return true;
-}else{
-for(let i=0;i<10;i++)if(!checkSat(arr[i],dim-1))return false;
-return true;
-}
-}
-function createEmpty(dim){
-if(dim===0)return [0,0,0,0,0,0,0,0,0,0];
-let arr=[];
-for(let i=0;i<10;i++)arr.push(createEmpty(dim-1));
-return arr;
-}
-function expand(){
-console.log("\n==> BIG BANG! UNIVERSE EXPANDED TO DIM: "+(maxDim+1));
-let newU=[];
-newU.push(universe);
-for(let i=1;i<10;i++)newU.push(createEmpty(maxDim));
-universe=newU;
-maxDim++;
-}
-function writeMem(coords,val){
-let t=universe;
-for(let i=coords.length-1;i>0;i--)t=t[coords[i]];
-t[coords[0]]=val;
-if(checkSat(universe,maxDim))expand();
-}
-function readMem(coords){
-let t=universe;
-for(let i=coords.length-1;i>=0;i--)t=t[coords[i]];
-return t;
-}
-function getNextPC(pc){
-let npc=[...pc];
-npc[0]++;
-if(npc[0]>9)npc[0]=0;
-return npc;
-}
-function getNextLoadPC(pc){
-let npc=[...pc];
-let i=0;
-while(i<npc.length){
-npc[i]++;
-if(npc[i]>9){
-npc[i]=0;
-i++;
-}else{
-break;
-}
-}
-if(i>=npc.length)npc.push(1);
-while(npc.length>maxDim+1)npc.pop();
-return npc;
-}
-function isIOPort(coords){
-if(coords.length!==maxDim+1)return false;
-for(let i=0;i<coords.length;i++){
-if(coords[i]!==9)return false;
-}
-return true;
-}
-function readStdin(){
+let mem=new Int32Array(1000);
+function r(a){
+if(a===999){
 let b=Buffer.alloc(1);
-try{
-let br=fs.readSync(0,b,0,1,null);
-if(br===0)return -1;
-console.log("\n[I/O] Read Stdin: "+String.fromCharCode(b[0]));
-return b[0];
-}catch(e){return -1;}
+try{if(fs.readSync(0,b,0,1,null)===0)return -1;return b[0];}catch(e){return -1;}
 }
-let pc;
-function readOperand(){
-let coords=[];
-for(let i=0;i<=maxDim;i++){
-coords.push(readMem(pc));
-pc=getNextPC(pc);
+return mem[(a%1000+1000)%1000];
 }
-return coords;
+function w(a,v){
+if(a===999){
+try{fs.writeSync(1,Buffer.from([v&255]));}catch(e){}
+}else{
+mem[(a%1000+1000)%1000]=v;
+}
 }
 function main(){
-console.log("Inceptionut VM - Bare Metal Execution");
-let filename = process.argv[2] || 'echo.inc';
-let rawCode;
-try{
-rawCode=fs.readFileSync(filename,'utf8');
-}catch(e){
-console.log("Failed to load native binary: " + filename);
-return;
+let rc;
+try{rc=fs.readFileSync(process.argv[2]||'encoder.inc','utf8');}catch(e){console.log("Failed to load native binary.");return;}
+let cc="";
+for(let i=0;i<rc.length;i++){
+if(rc[i]===' '||rc[i]==='S')cc+="0";
+if(rc[i]==='\t'||rc[i]==='T')cc+="1";
 }
-let cleanCode="";
-for(let i=0;i<rawCode.length;i++){
-if(rawCode[i]===' '||rawCode[i]==='S')cleanCode+="0";
-if(rawCode[i]==='\t'||rawCode[i]==='T')cleanCode+="1";
+for(let i=0;i<cc.length;i+=4){
+let b=cc.substring(i,i+4);
+let v=0;
+if(b[0]==='1')v+=8;if(b[1]==='1')v+=4;if(b[2]==='1')v+=2;if(b[3]==='1')v+=1;
+mem[Math.floor(i/4)%1000]=v;
 }
-let blocks=[];
-let cur="";
-for(let i=0;i<cleanCode.length;i++){
-cur+=cleanCode[i];
-if(cur.length===4){
-blocks.push(cur);
-cur="";
-}
-}
-let loadPC=[0];
-for(let i=0;i<blocks.length;i++){
-let b=blocks[i];
-let val=0;
-if(b[0]==='1')val+=8;
-if(b[1]==='1')val+=4;
-if(b[2]==='1')val+=2;
-if(b[3]==='1')val+=1;
-let prevMax=maxDim;
-writeMem(loadPC,val);
-if(maxDim>prevMax){
-let newPC=new Array(maxDim+1).fill(0);
-newPC[maxDim]=1;
-loadPC=newPC;
-}else{
-loadPC=getNextLoadPC(loadPC);
-}
-}
-pc=new Array(maxDim+1).fill(0);
-let cycles=0;
-let maxCycles=50;
+let pc=0;
+let c=0;
 console.log("Starting Subleq Execution Loop...");
-while(cycles<maxCycles){
-let currentPC=[...pc];
-let coordA=readOperand();
-let coordB=readOperand();
-let coordC=readOperand();
-let valA;
-if(isIOPort(coordA)){
-valA=readStdin();
-}else{
-valA=readMem(coordA);
-}
-let valB=isIOPort(coordB)?0:readMem(coordB);
-let res=valB-valA;
-if(isIOPort(coordB)){
-console.log("\n[I/O] Write Stdout: "+String.fromCharCode(res&255));
-writeMem(coordB,res);
-}else{
-writeMem(coordB,res);
-}
-console.log("Cycle "+cycles+": Subleq("+JSON.stringify(coordA)+","+JSON.stringify(coordB)+","+JSON.stringify(coordC)+")");
+while(c++<99999){
+let ax=r(pc),ay=r(pc+1),az=r(pc+2),bx=r(pc+3),by=r(pc+4),bz=r(pc+5),cx=r(pc+6),cy=r(pc+7),cz=r(pc+8);
+let aa=ax===9&&ay===9&&az===9?999:ax+ay*10+az*100;
+let ab=bx===9&&by===9&&bz===9?999:bx+by*10+bz*100;
+let ac=cx===9&&cy===9&&cz===9?999:cx+cy*10+cz*100;
+let va=r(aa);
+let vb=ab===999?0:r(ab);
+let res=vb-va;
+w(ab,res);
 if(res<=0){
-if(JSON.stringify(currentPC)===JSON.stringify(coordC)){
-console.log("Halt Condition: Infinite Loop Detected at "+JSON.stringify(currentPC));
-break;
+if(pc===ac){console.log("Halt Condition Reached.");break;}
+pc=ac;
+}else{
+pc=(Math.floor(pc/10)*10+10)%1000;
 }
-pc=[...coordC];
-console.log("  Jumped to PC: "+JSON.stringify(pc));
 }
-cycles++;
-}
-console.log("Execution Terminated.");
 }
 main();
